46bb467fe56ede81a56e4b9909abf5a8c9325000
devarsh shah <devarshmshah@gmail.com>
2020-11-10 12:31:06 +0530

    changes

--- src/app/crm/app.tsx
+++ src/app/crm/app.tsx
@@ -1,11 +1,12 @@
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import { theme } from "./theme";
-import IndexPage from "pages_crm";
 import "registry/fns";
 import "registry/yup";
 import { APISDK } from "registry/fns/sdk";
+import IndexPage from "pages_crm";
 APISDK.createSession(process.env.REACT_APP_API_URL ?? "");
 
 const themeObj = createMuiTheme(theme);
