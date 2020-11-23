512c1c05e725f77aa27d048ba1a9484d28164aff
devarsh shah <devarshmshah@gmail.com>
2020-11-23 13:15:46 +0530

    changes

--- src/pages_los/index.tsx
+++ src/pages_los/index.tsx
@@ -44,15 +44,3 @@ const IndexPage = () => {
 };
 
 export default IndexPage;
-
-function Child() {
-  // We can use the `useParams` hook here to access
-  // the dynamic pieces of the URL.
-  let { id } = useParams();
-
-  return (
-    <div>
-      <h3>ID: {id}</h3>
-    </div>
-  );
-}
