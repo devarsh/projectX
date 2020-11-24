a395da8d595ea1520ad2565b551f7f9b34638694
Devarsh Shah <devarshmshah@gmail.com>
2020-11-24 18:04:39 +0530

    Update sdk.ts

--- src/registry/fns/sdk.ts
+++ src/registry/fns/sdk.ts
@@ -56,7 +56,6 @@ const RaatnaFinAPI = () => {
     });
   };
   const verifyRequest = (data) => {
-    // console.log("data", data);
     if (data["access_token"] && data["refresh_token"]) {
       sessionObj.loginStatus = true;
       sessionObj.token = data;
