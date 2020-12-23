70e8e323e1545ca2bfc8d1b5668407ab5106385d
Devarsh Shah <devarshmshah@gmail.com>
2020-12-23 14:00:00 +0530

    los changes

--- src/pages_los/pages/tabView/gridTabPanel.tsx
+++ src/pages_los/pages/tabView/gridTabPanel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { App } from "../../../components/dataTable/test";
+import { ParentGridWrapper } from "../../../components/dataTable";
 
 export const GridTabPanel = () => {
-  return <App />;
+  return <ParentGridWrapper />;
 };
