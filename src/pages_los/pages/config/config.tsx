d4ea15235c3f1fcde0a2609b7660a4aa4525a352
Devarsh Shah <devarshmshah@gmail.com>
2021-02-16 18:09:33 +0530

    changes

--- src/pages_los/pages/config/config.tsx
+++ src/pages_los/pages/config/config.tsx
@@ -2,21 +2,47 @@ import { useRef, Fragment } from "react";
 import { GridCRUD } from "pages_los/common/crud2";
 import { BankMasterSMEDetailsMetaData } from "registry/metaData";
 import { BankMasterGridMetaData } from "registry/metaData/grid";
+import { CRUDContextProvider } from "pages_los/common/crud2";
+import { LOSSDK } from "registry/fns/los";
 
 export const Config = () => {
-  let refID = 89;
   const isProductEditedRef = useRef(false);
+  const crudAPIArgs = (moduleType, productType, refID) => ({
+    insertFormData: {
+      fn: LOSSDK.insertFormData,
+      args: { moduleType, productType, refID },
+    },
+    checkFormDataExist: {
+      fn: LOSSDK.checkFormDataExist,
+      args: { moduleType, productType, refID },
+    },
+    deleteFormData: {
+      fn: LOSSDK.deleteFormData,
+      args: { moduleType, productType, refID },
+    },
+    updateFormData: {
+      fn: LOSSDK.updateFormData,
+      args: { moduleType, productType, refID },
+    },
+    getFormData: {
+      fn: LOSSDK.getFormData,
+      args: { moduleType, productType, refID },
+    },
+    getStaticGridData: {
+      fn: LOSSDK.getStaticGridData,
+      args: { moduleType, productType, refID },
+    },
+  });
 
   return (
     <Fragment>
-      {/* <GridCRUD
-        refID={refID}
-        moduleType={"config/bank"}
-        productType={"sme"}
-        isProductEditedRef={isProductEditedRef}
-        formMetaData={BankMasterSMEDetailsMetaData}
-        gridMetaData={BankMasterGridMetaData}
-      /> */}
+      <CRUDContextProvider {...crudAPIArgs("config/bank", "sme", null)}>
+        <GridCRUD
+          isProductEditedRef={isProductEditedRef}
+          formMetaData={BankMasterSMEDetailsMetaData}
+          gridMetaData={BankMasterGridMetaData}
+        />
+      </CRUDContextProvider>
     </Fragment>
   );
 };
