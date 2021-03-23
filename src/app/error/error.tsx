7c375c32071a5b8a771e313fd1d325ba452d8475
Devarsh Shah <devarshmshah@gmail.com>
2021-03-23 16:18:04 +0530

    error page goback button added

--- src/app/error/error.tsx
+++ src/app/error/error.tsx
@@ -1,10 +1,15 @@
 import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 import SuccessImg from "assets/images/success.svg";
+import { useNavigate } from "react-router";
 import { useStyles } from "./style";
 
 export const ErrorPage = () => {
   const classes = useStyles();
-
+  const navigate = useNavigate();
+  const returnToHomePage = () => {
+    navigate("/crm");
+  };
   return (
     <Box
       className={classes.wrapper}
@@ -16,6 +21,7 @@ export const ErrorPage = () => {
       <Box className={classes.center} mt={3} mb={3}>
         <h3 className="theme-color2">Something Unexpected Happend</h3>
       </Box>
+      <Button onClick={returnToHomePage}>Return to Home Page</Button>
     </Box>
   );
 };
