09651d59dd2334d768c4078f01da8ce05872e7f6
Devarsh Shah <devarshmshah@gmail.com>
2021-04-06 12:52:07 +0530

    changes

--- startServer.sh
+++ startServer.sh
@@ -1,1 +1,2 @@
-http-server ./build -S true -C ./
+. /home/oracle/.nvm/nvm.sh
+cd /home/oracle/Ratnafin && http-server ./build --proxy http://192.168.1.132:3000? --cert ./AIPLCert.crt --key ./AIPLprivateKey.pem --port 3000 --ssl true
