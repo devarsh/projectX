958348884d586ea0d8a15a0082f0dab3986f0f9f
Devarsh Shah <devarshmshah@gmail.com>
2021-04-30 13:56:46 +0530

    changes

---
+++ src/pages_verification/pages/equifax/api.ts
@@ -0,0 +1,91 @@
+import { VerificationSDK } from "registry/fns/verification";
+
+export const requestEquifaxScore = async (
+  tokenID: number | string,
+  consent: string
+) => {
+  const { data, status } = await VerificationSDK.internalFetcher(
+    "./equifax/request/send",
+    {
+      body: JSON.stringify({
+        request_data: {
+          tokenID: tokenID,
+          consent: consent,
+        },
+        channel: "W",
+      }),
+    }
+  );
+  if (status === "success") {
+    return data?.response_data;
+  } else {
+    throw data?.error_data;
+  }
+};
+
+export const requestOTP = async (
+  tokenID: number | string,
+  mobileNo: string
+) => {
+  const { data, status } = await VerificationSDK.internalFetcher(
+    "./equifax-otp/mobile/send",
+    {
+      body: JSON.stringify({
+        request_data: {
+          tokenID: tokenID,
+          mobileNo: mobileNo,
+        },
+        channel: "W",
+      }),
+    }
+  );
+  if (status === "success") {
+    return data?.response_data;
+  } else {
+    throw data?.error_data;
+  }
+};
+
+export const verifyOTP = async (
+  tokenID: number | string,
+  transactionID: string,
+  otp: string
+) => {
+  const { data, status } = await VerificationSDK.internalFetcher(
+    "./equifax-otp/mobile/verify",
+    {
+      body: JSON.stringify({
+        request_data: {
+          tokenID: tokenID,
+          otp: otp,
+          transactionID: transactionID,
+        },
+        channel: "W",
+      }),
+    }
+  );
+  if (status === "success") {
+    return data?.response_data;
+  } else {
+    throw data?.error_data;
+  }
+};
+
+export const verifyToken = async (tokenID: number | string) => {
+  const { data, status } = await VerificationSDK.internalFetcher(
+    "./equifax/token/verify",
+    {
+      body: JSON.stringify({
+        request_data: {
+          tokenID: tokenID,
+        },
+        channel: "W",
+      }),
+    }
+  );
+  if (status === "success") {
+    return data?.response_data;
+  } else {
+    throw data?.error_data;
+  }
+};
