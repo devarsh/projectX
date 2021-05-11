45fe208a257d4f8e8ca253105ceeddd04672e084
Devarsh Shah <devarshmshah@gmail.com>
2021-05-12 01:28:19 +0530

    changes

--- src/pages_los/pages/profile/profile.tsx
+++ src/pages_los/pages/profile/profile.tsx
@@ -9,15 +9,21 @@ import { ChangePassword } from "./changePassword";
 import loaderGif from "assets/images/loader.gif";
 import { UserProfileMetaData } from "./metaData";
 import * as API from "./api";
+import { useEffect } from "react";
+import { queryClient } from "cache";
 
 export const Profile = () => {
   const [showProfile, setShowProfile] = useState(false);
   const userData = useContext(AuthContext);
   const userID = userData?.authState?.userId;
-  const queryData = useQuery<any, any, any>("getProfileData", () =>
+  const queryData = useQuery<any, any, any>(["getEmployeeProfile"], () =>
     API.getEmployeeProfile({ userID })
   );
-
+  useEffect(() => {
+    return () => {
+      queryClient.removeQueries(["getEmployeeProfile"]);
+    };
+  }, []);
   return queryData.isLoading || queryData.isFetching ? (
     <Fragment>
       <img src={loaderGif} alt="loader" width="50px" height="50px" />
@@ -25,6 +31,7 @@ export const Profile = () => {
   ) : queryData.isError ? (
     <Fragment>
       <Alert
+        severity="error"
         errorMsg={queryData.error?.error_msg ?? "Unknown error occured"}
         errorDetail={queryData.error?.error_detail ?? ""}
       />
