b05d5396ff4609c9bf5408e31329d6e441d9a8d6
Raveena <gitguest2@acuteinformatics.in>
2021-05-13 16:20:20 +0530

    CAM CSS UI Updates

--- src/app/middleware/cam.tsx
+++ src/app/middleware/cam.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { CAM } from "pages_los/pages/cam";
 import { MiddlewareSDK } from "registry/fns/middleware";
 import loaderGif from "assets/images/loader.gif";
+import logo from "assets/images/logo.svg";
 import { useParams } from "react-router-dom";
 
 export const CAMMiddlewareWrapper = () => {
@@ -19,7 +20,18 @@ export const CAMMiddlewareWrapper = () => {
       }
     </span>
   ) : (
-    <CAM camData={result.data} />
+    <div
+      style={{
+        maxWidth: "230mm",
+        margin: "0 auto",
+        border: "2px solid #ddd",
+        padding: "10px",
+        background: "#fff",
+      }}
+    >
+      <img src={logo} style={{ display: "flex", margin: "0 auto 10px auto" }} />
+      <CAM camData={result.data} />
+    </div>
   );
   return renderResult;
 };
--- src/pages_los/pages/lead/cam/headerDetails.tsx
+++ src/pages_los/pages/lead/cam/headerDetails.tsx
@@ -8,33 +8,31 @@ export const HeaderDetails = ({ rowData }) => {
     <Box
       display="flex"
       flexDirection="column"
-      paddingLeft={4}
-      paddingRight={4}
       paddingTop={1}
       paddingBottom={1}
       width={1}
     >
       <Box display="flex" flexDirection="row" width={1} mb={2}>
-        <Box pr={3}>
+        <Box pr={1}>
           <div className={classes.labelText}>LeadNo</div>
           <div className={classes.valueText}>{rowData?.refID}</div>
         </Box>
-        <Box px={3}>
+        <Box px={1}>
           <div className={classes.labelText}>Category Name</div>
           <div className={classes.valueText}>{rowData?.categoryName}</div>
         </Box>
-        <Box px={3}>
+        <Box px={1}>
           <div className={classes.labelText}>Product Name</div>
           <div className={classes.valueText}>{rowData?.productName}</div>
         </Box>
         {rowData?.subProduct1Name !== "" ? (
-          <Box px={3}>
+          <Box px={1}>
             <div className={classes.labelText}>Sub Product1 Name</div>
             <div className={classes.valueText}>{rowData?.subProduct1Name}</div>
           </Box>
         ) : null}
         {rowData?.subProduct2Name !== "" ? (
-          <Box px={3}>
+          <Box pl={1}>
             <div className={classes.labelText}>Sub Product2 Name</div>
             <div className={classes.valueText}>{rowData?.subProduct2Name}</div>
           </Box>
